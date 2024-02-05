5a6a5e5974374a8935b04cf6b6715e20ee49ecc0
kaushal <kaushalagarwal1997@gmail.com>
2024-02-04 21:42:09 -0500

    module example in nodejs

---
+++ index.js
@@ -0,0 +1,10 @@
+var rect = require("./rectangle");
+
+// var rect = {
+//     perimeter: (x,y) => (2*(x+y)),
+//     area: (x,y) => (x*y)
+// }
+
+
+console.log("Perimeter of the Rectangle",rect.perimeter(3,4));
+console.log("Area of the Rectangle",rect.area(3,4));
