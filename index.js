3b5bfab87cf2c7866741a2e3eb0922700c042220
kaushal <kaushalagarwal1997@gmail.com>
2024-02-04 22:03:46 -0500

    callback and error handling.

--- index.js
+++ index.js
@@ -1,10 +1,33 @@
 var rect = require("./rectangle");
 
-// var rect = {
-//     perimeter: (x,y) => (2*(x+y)),
-//     area: (x,y) => (x*y)
-// }
+function solveRect(l,b){
+    console.log("Solving for rectangle with l = "+l+" and b = "+b);
+    rect(l,b,(err,rectangle)=>{
+        if(err){
+            console.log("Error: ", err.message);
+        }else{
+                console.log("The area of the rectangle of dimensions l = "
+                    + l + " and b = " + b + " is " + rectangle.area());
+                console.log("The perimeter of the rectangle of dimensions l = "
+                    + l + " and b = " + b + " is " + rectangle.perimeter());
+        }
+    });
+    console.log("this statement after the call to rect()");
+}
 
+function showName(name){
+    rect.printName(name, (err, statement)=>{
+        if(err){
+            console.log("Getting error");
+        }else{
+            console.log(statement);
+        }
+    }) 
 
-console.log("Perimeter of the Rectangle",rect.perimeter(3,4));
-console.log("Area of the Rectangle",rect.area(3,4));
+}
+
+showName("Kaushal Agarwal");
+solveRect(2,4);
+solveRect(3,5);
+solveRect(0,5);
+solveRect(-3,5);
